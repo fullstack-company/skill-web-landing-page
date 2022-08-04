c7fba29703343c3ba3ebc264ae24950a5e134834
Fedor Sharaputo <sharaputo@icloud.com>
2022-08-04 12:48:11 +0300

    header styles mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const menu = document.getElementById("menu");
+
+  if (burger) {
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("_active");
+      menu.classList.toggle("_active");
+
+      if (burger.classList.contains("_active")) {
+        document.body.style.overflow = "hidden";
+        document.body.style.height = "100vh";
+      } else {
+        document.body.style.overflow = null;
+        document.body.style.height = null;
+      }
+    })
+  }
+});
